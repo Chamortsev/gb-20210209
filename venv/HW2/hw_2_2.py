64655c7a673817d10670103551c72dbbc5c751b7
Vladimir <chamorsev@mail.ru>
2021-02-12 19:14:31 +0300

    HW 2 Из методички

--- venv/HW2/hw_2_2.py
+++ venv/HW2/hw_2_2.py
@@ -1,31 +1,35 @@
 a = ['в', '5', 'часов', '17', 'минут', 'температура', 'воздуха', 'была', '+5', 'градусов']
-c=''
+c = ''
+count=0
 # Не понравился вариант реализации
 # a[1]='05'
 # a[8]='+05'
-# a.insert(1,'"')
-# a.insert(3,'"')
-# a.insert(5,'"')
-# a.insert(7,'"')
-# a.insert(-1,'"')
-# a.insert(-3,'"')
-#
-# text = ''.join(a)
-# print(text)
-for key,i in enumerate(a):
-    if (i[0] == '+'):
-        b = i.replace('+','')
-        c = '+'
+a.insert(1, '"')
+a.insert(3, '"')
+a.insert(5, '"')
+a.insert(7, '"')
+a.insert(-1, '"')
+a.insert(-3, '"')
+
+text = ' '.join(a)
+
+for key, i in enumerate(a):
+    if i[0] == '+':
+         b = i.replace('+', '')
+         c = '+'
+    elif i[0] =='"':
+        b=''
     else:
         b = i
-    if(b.isdigit() == True ):
-        if (len(b) == 1):
+    if b.isdigit():
+        if len(b) == 1:
             msg = f'"{c}0{b}"'
             print(msg, end=' ')
-            #print('"' + c + '0' + b + '"', end=' ')
+            # print('"' + c + '0' + b + '"', end=' ')
         else:
-            msg=f'"{c}{b}"'
+            msg = f'"{c}{b}"'
             print(msg, end=' ')
-            #print('"' + c + b + '"', end=' ')
+            # print('"' + c + b + '"', end=' ')
     else:
         print(b, end=' ')
+
